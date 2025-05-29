3854d479da68291b6de9baa613878ee8e96c3bec
yazcity <yaseenvk1990@gmail.com>
2025-05-29 18:24:55 +0400

    router

--- src/Components/LayoutRoutes/LayoutRoutes.jsx
+++ src/Components/LayoutRoutes/LayoutRoutes.jsx
@@ -13,6 +13,7 @@ function LayoutRoutes() {
     <Router>
         <Header/>
             <Routes>
+            <Route path='/my-first-app' element={ <Home/> }/>
             <Route path='/' element={ <Home/> }/>
             <Route path='/dishes' element={ <Dishes/> }/>
             <Route path='/services' element={ <Booking/> }/>
